563d75aec9fa3b2795b9d789577ea3e8344fd6f1
Dan Corkill <daniel.corkill@gmail.com>
2012-12-30 22:39:49 +0200

    added validation posts / comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,13 @@ class CommentsController < ApplicationController
 	def create
 		@post = Post.find_by_slug(params[:post_id])
 		@comment = @post.comments.create(params[:comment])
-		redirect_to post_path(@post), :notice => 'Boom left comment'
+
+		respond_to do |f|
+			if @comment.save
+				f.html { redirect_to post_path(@post), :notice => 'Boom left comment' }
+			else
+				f.html { redirect_to post_path(@post), :alert => 'Comment must be at least 5 characters.' }
+			end
+		end
 	end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
 	attr_accessible :content, :post_id
 
 	belongs_to :post
+
+	validates :content, presence: true
+	validates :content, length: { minimum: 5 }
 end
